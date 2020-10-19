4fe59c2a94c8c9967526c1b8e6e4a2ce8c278649
MHusna <48207418+MHusna@users.noreply.github.com>
2020-10-19 21:13:41 +0300

    Add files via upload

---
+++ calendar_month.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 20:35:50 2020
+
+@author: mehme
+"""
+import calendar 
+
+yy = 2020
+mm = 10
+
+print(calendar.month(yy, mm))
---
+++ calendar_year.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 20:59:46 2020
+
+@author: mehme
+"""
+
+import calendar 
+
+print("2020 Yılı Takvimi: ")
+print(calendar.calendar(2020, 2, 1, 5))
+print(iterweekdays())
